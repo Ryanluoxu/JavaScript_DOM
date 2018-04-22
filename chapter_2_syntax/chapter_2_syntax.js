b82b0b93e74320de5e70a0e302a356c24e23da2d
ryanluoxu <luoxu2011@gmail.com>
2018-04-22 10:43:53 +0800

    initial commit

---
+++ chapter_2_syntax/chapter_2_syntax.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+
+// # Syntax
+
+// ## Array
+
+var beatles = ["john","paul","george","ringo"];
+//alert(beatles[1]);
+
+var ryan = [30, "singing"];
+var melvin = [33, "watching video"];
+var myTeam = [ryan, melvin];
+alert(myTeam[0][1]);
+
+// ## Object
+var lennon = Object();
+
